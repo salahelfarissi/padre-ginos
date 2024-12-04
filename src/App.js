3a5828d31d6f1abd12bdd7dc1bdeca1482b9559b
salahelfarissi <salaheddine.elfarissi@gmail.com>
2024-12-04 19:54:20 +0100

    Add pizza component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+const Pizza = (props) => {
+	return React.createElement('div', {}, [
+		React.createElement('h1', {}, props.name),
+		React.createElement('p', {}, props.description),
+	])
+}
+
 const App = () => {
 	return React.createElement(
 		'div',
 		{},
-		React.createElement('h1', {}, "Padre Gino's")
+		[
+			React.createElement('h1', {}, "Padre Gino's"),
+			// Pizza, {}, "", we can omit
+			React.createElement(Pizza, {
+				name: 'Pepperoni',
+				description: 'Pepperoni, cheese, tomato sauce',
+			}),
+			React.createElement(Pizza, {
+				name: 'Margherita',
+				description: 'Cheese, tomato sauce, basil',
+			}),
+			React.createElement(Pizza, {
+				name: 'Hawaiian',
+				description: 'Ham, pineapple, cheese, tomato sauce',
+			}),
+			React.createElement(Pizza, {
+				name: 'Vegetarian',
+				description: 'Mushrooms, bell peppers, onions, olives, cheese, tomato sauce'
+			})
+		]
 	)
 }
 
