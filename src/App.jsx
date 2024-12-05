d771253c9d81107a02962aecede9e48c513b36de
salahelfarissi <salaheddine.elfarissi@gmail.com>
2024-12-05 07:33:02 +0100

    Add Order component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="pep, cheese, sauce"
-        image="/public/pizzas/pepperoni.webp"
-      />
-      <Pizza
-        name="Hawaiian"
-        description="ham, pineapple, cheese, sauce"
-        image="/public/pizzas/hawaiian.webp"
-      />
-      <Pizza
-        name="Americano"
-        description="french fries, hot dogs, cheese, sauce"
-        image="/public/pizzas/big_meat.webp"
-      />
+      <Order />
     </div>
   );
 };
---
+++ src/Order.jsx
@@ -0,0 +1,69 @@
+import Pizza from "./Pizza";
+
+const Order = () => {
+  const pizzaType = "pepperoni";
+  const pizzaSize = "M";
+  return (
+    <div className="order">
+      <h2>Create Order</h2>
+      <form>
+        <div>
+          <div>
+            <label htmlFor="pizza-type">Pizza Type</label>
+            <select name="pizza-type" value={pizzaType}>
+              <option value="pepperoni">Pepperoni</option>
+              <option value="hawaiian">Hawaiian</option>
+              <option value="big_meat">Americano</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="pizza-size">Pizza Size</label>
+            <div>
+              <span>
+                <input
+                  checked={pizzaSize === "S"}
+                  type="radio"
+                  name="pizza-size"
+                  value="S"
+                  id="pizza-s"
+                />
+                <label htmlFor="pizza-s">Small</label>
+              </span>
+              <span>
+                <input
+                  checked={pizzaSize === "M"}
+                  type="radio"
+                  name="pizza-size"
+                  value="M"
+                  id="pizza-m"
+                />
+                <label htmlFor="pizza-m">Medium</label>
+              </span>
+              <span>
+                <input
+                  checked={pizzaSize === "L"}
+                  type="radio"
+                  name="pizza-size"
+                  value="L"
+                  id="pizza-l"
+                />
+                <label htmlFor="pizza-l">Large</label>
+              </span>
+            </div>
+          </div>
+          <button type="submit">Add to Cart</button>
+          <div className="order-pizza">
+            <Pizza
+              name="Pepperoni"
+              description="pep, cheese, sauce"
+              image="/public/pizzas/pepperoni.webp"
+            />
+            <p>$13.37</p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Order;
