b37ca6bad87dd23f7f236f8efb533768eda44088
salahelfarissi <salaheddine.elfarissi@gmail.com>
2024-12-07 16:42:27 +0100

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
 import PizzaOfTheDay from "./PizzaOfTheDay";
+import Header from "./Header";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1 className="logo">Padre Gino's - Order Now</h1>
+        <Header />
         <Order />
         <PizzaOfTheDay />
       </div>
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+const headers = () => {
+  return (
+    <nav>
+      <h1 className="logo"> Padre Gino's Pizza</h1>
+      <div className="nav-cart">
+        🛒<span className="nav-cart-number">5</span>
+      </div>
+    </nav>
+  );
+};
+
+export default headers;
