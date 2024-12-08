e588b40a31159710ba0be09fa817f16f35d3e2e4
salahelfarissi <salaheddine.elfarissi@gmail.com>
2024-12-08 16:46:13 +0100

    Test pizza of the day api call

--- src/__tests__/usePizzaOfTheDay.test.jsx
+++ src/__tests__/usePizzaOfTheDay.test.jsx
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { expect, test, vi } from "vitest";
 import createFetchMock from "vitest-fetch-mock";
 import usePizzaOfTheDay from "../usePizzaOfTheDay";
@@ -20,4 +20,11 @@ test("gives null when first called", async () => {
   fetchMocker.mockResponseOnce(JSON.stringify(testPizza));
   const { result } = renderHook(() => usePizzaOfTheDay());
   expect(result.current).toBe(null);
+});
+
+test("to call the API and give back the pizza of the day", async () => {
+  fetchMocker.mockResponseOnce(JSON.stringify(testPizza));
+  const { result } = renderHook(() => usePizzaOfTheDay());
+  await waitFor(() => expect(result.current).toEqual(testPizza));
+  expect(fetchMocker).toBeCalledWith("/api/pizza-of-the-day");
 });
